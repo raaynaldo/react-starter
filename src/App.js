2d73da2d8bba844aac0871bb5eab4737608d59fd
Raynaldo Sutisna <raaynaldo@gmail.com>
2022-10-30 22:45:15 -0400

    feat: remove loading

--- src/App.js
+++ src/App.js
@@ -24,21 +24,6 @@ export default function App() {
           Disabled
         </Button>
       </div>
-
-      <div style={{ margin: '10px', display: 'flex', gap: '20px' }}>
-        <Button variant='primary' disabled={true} isLoading={true}>
-          Disabled
-        </Button>
-        <Button variant='secondary' disabled={true} isLoading={true}>
-          Disabled
-        </Button>
-        <Button variant='outline' disabled={true} isLoading={true}>
-          Disabled
-        </Button>
-        <Button variant='ghost' disabled={true} isLoading={true}>
-          Disabled
-        </Button>
-      </div>
     </div>
   );
 }
--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,52 +1,17 @@
 import React from 'react';
 import clsx from 'clsx';
-import { FaSpinner } from 'react-icons/fa';
 
 import styles from './Button.module.css';
-const ButtonVariant = Object.freeze({
-  Primary: 'primary',
-  Secondary: 'secondary',
-  Outline: 'outline',
-  Ghost: 'ghost',
-});
 
 export default function Button(props) {
-  const {
-    children,
-    className = '',
-    disabled: buttonDisabled,
-    isLoading,
-    variant = 'primary',
-    ...rest
-  } = props;
-
-  const disabled = buttonDisabled || isLoading;
+  const { children, className = '', variant = 'primary', ...rest } = props;
 
   return (
     <button
       variant={variant}
-      className={clsx(
-        styles.button,
-        styles[variant],
-        isLoading && styles.loading,
-        className
-      )}
-      disabled={disabled}
+      className={clsx(styles.button, styles[variant], className)}
       {...rest}
     >
-      {isLoading && (
-        <div className={styles.spinner}>
-          <FaSpinner
-            className={clsx(styles.animateSpin, [
-              [ButtonVariant.Primary].includes(variant) && styles.textWhite,
-              [ButtonVariant.Secondary, ButtonVariant.Ghost].includes(
-                variant
-              ) && styles.textBlue,
-              [ButtonVariant.Outline].includes(variant) && styles.textGray,
-            ])}
-          />
-        </div>
-      )}
       {children}
     </button>
   );
